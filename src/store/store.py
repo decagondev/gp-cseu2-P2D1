6ffbca70aa8cd4224cefad3cee16c871f0e02900
Tom <tomtarpeydev@gmail.com>
2019-10-02 10:24:45 +0100

    itteration 1 of store calss

--- src/store/store.py
+++ src/store/store.py
@@ -1,1 +1,24 @@
 # Create a store class with a name and categories
+# fields name, categories
+
+class Store:
+    # constructor
+    def __init__(self, name, categories):
+        self.name = name
+        self.categories = categories
+    # methods
+    def __str__(self):
+        output = ""
+        output += self.name + "\n"
+        i = 1
+        for c in self.categories:
+            output += "  " + str(i) + ". " + c + "\n"
+            i += 1
+        return output
+
+s = Store("Bob's Store", ["Shoes", "Hats", "Hellicopters"])
+
+print(s)
+selection = input("Select the number of the department.")
+
+print("The user selected " + str(selection))

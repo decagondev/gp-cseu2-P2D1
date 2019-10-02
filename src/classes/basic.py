8f40d4e259f30ecba478c8c2c2c9e88467c9d8ef
Tom <tomtarpeydev@gmail.com>
2019-10-02 09:39:15 +0100

    Animals and dogs

--- src/classes/basic.py
+++ src/classes/basic.py
@@ -1,1 +1,40 @@
 # create a basic Animal class add some getter and setter methods
+class Animal:
+    # constructor
+    def __init__(self, name, num_legs):
+        self.name = name
+        self.num_legs = num_legs
+    # methods
+
+    # getters
+    def get_name(self):
+        return self.name
+
+    def get_legs(self):
+        return self.num_legs
+    # setters
+    def set_name(self, name):
+        self.name = name
+
+    def set_legs(self, num_legs):
+        self.num_legs = num_legs
+
+# "is a"
+class Dog(Animal):
+    def __init__(self, breed, num_legs=4):
+        self.name = "Dog"
+        self.breed = breed
+        self.num_legs = num_legs
+    
+    # getters and setters
+
+    def get_breed(self):
+        return self.breed
+    
+    def set_breed(self, breed):
+        self.breed = breed
+
+
+a = Animal("Bob", 20)
+a.name = "Dave"
+a.age = 23

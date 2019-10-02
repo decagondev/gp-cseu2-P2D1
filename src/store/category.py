7a043663670ed1680730c20a8edf29c1d965103f
Tom <tomtarpeydev@gmail.com>
2019-10-02 10:43:52 +0100

    added category class

--- src/store/category.py
+++ src/store/category.py
@@ -1,2 +1,11 @@
 # create a category class with a name for now 
 # and build upon it further in future iterations
+
+class Category:
+
+    # constructor
+    def __init__(self, name): #, products)
+        self.name = name
+    # str
+    def __str__(self):
+        return "No products available in " + self.name

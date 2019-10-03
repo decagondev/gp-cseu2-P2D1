ba223683fbd0416275c957321c3d058faa3cc618
Tom <tomtarpeydev@gmail.com>
2019-10-03 10:21:30 +0100

    simple composition

--- src/classes/simple_composition.py
+++ src/classes/simple_composition.py
@@ -1,1 +1,17 @@
 # create a Motor class and create a Car class that "Has A" Motor
+
+class Motor:
+    def __init__(self, make, cc):
+        self.make = make
+        self.cc = cc
+
+class Car:
+    def __init__(self, make, model, engine):
+        self.make = make
+        self.model = model
+        self.engine = engine
+        
+honda_petrol_motor = Motor("Honda", 1799)
+car = Car("Honda", "Civic", honda_petrol_motor)
+
+print(car.engine.cc)
